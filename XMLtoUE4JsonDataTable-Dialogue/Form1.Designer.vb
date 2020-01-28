3a3a3cacc3091acd03597290d7d8c3dbf084444d
navima <draco.109@gmail.com>
2020-01-28 10:24:12 +0100

    Fixed Groupnode at write

--- XMLtoUE4JsonDataTable-Dialogue/Form1.Designer.vb
+++ XMLtoUE4JsonDataTable-Dialogue/Form1.Designer.vb
@@ -22,10 +22,47 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.InputsFilesContainer = New System.Windows.Forms.FlowLayoutPanel()
+        Me.ConvertButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'InputsFilesContainer
+        '
+        Me.InputsFilesContainer.AllowDrop = True
+        Me.InputsFilesContainer.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.InputsFilesContainer.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.InputsFilesContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.InputsFilesContainer.Cursor = System.Windows.Forms.Cursors.Default
+        Me.InputsFilesContainer.Location = New System.Drawing.Point(12, 12)
+        Me.InputsFilesContainer.Name = "InputsFilesContainer"
+        Me.InputsFilesContainer.Size = New System.Drawing.Size(317, 426)
+        Me.InputsFilesContainer.TabIndex = 0
+        '
+        'ConvertButton
+        '
+        Me.ConvertButton.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ConvertButton.Location = New System.Drawing.Point(335, 12)
+        Me.ConvertButton.Name = "ConvertButton"
+        Me.ConvertButton.Size = New System.Drawing.Size(453, 426)
+        Me.ConvertButton.TabIndex = 1
+        Me.ConvertButton.Text = "Convert"
+        Me.ConvertButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ConvertButton)
+        Me.Controls.Add(Me.InputsFilesContainer)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents InputsFilesContainer As FlowLayoutPanel
+    Friend WithEvents ConvertButton As Button
 End Class
